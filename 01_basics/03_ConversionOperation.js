dabe9e9b190ab997c8b19e0f109aff48d7b56a31
prafful sharma <sharmaprafful646@gmail.com>
2024-12-19 19:25:53 +0000

    main

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -9,5 +9,11 @@ let score = false
 //console.log(valueInNumber);
 //true= 1,false = 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
