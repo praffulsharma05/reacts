94eddb3de932d260140b47083c59ae2c8022518e
prafful sharma <sharmaprafful646@gmail.com>
2024-12-19 19:47:24 +0000

    main

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -14,6 +14,31 @@ let score = false
 // console.log(booleanIsLoggedIn);
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+//**************************************Operations *********************************************/
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = "hello"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+
+console.log("1"+ 2+3);
+
+console.log(1+2+"2");
+
+
