44257ad1755c0fb0626be94d62a3cb50baf032b4
prafful sharma <sharmaprafful646@gmail.com>
2024-12-23 09:40:16 +0000

    main

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -26,19 +26,31 @@ let score = false
 // console.log(negValue);
 
 
-let str1 = "hello"
-let str2 = "hello"
+// let str1 = "hello"
+// let str2 = "hello"
 
-let str3 = str1 + str2
-console.log(str3);
+// let str3 = str1 + str2
+// console.log(str3);
 
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+2+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
 
-console.log("1"+ 2+3);
+// console.log("1"+ 2+3);
 
-console.log(1+2+"2");
+// console.log(1+2+"2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2  + num3
+
+let gameCounter=100
+gameCounter;
+console.log(gameCounter);
+
 
 
