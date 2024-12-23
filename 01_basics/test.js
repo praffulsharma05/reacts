3dadf9a4dd52d8bb6adf77d99682c8139d2e54b2
prafful sharma <sharmaprafful646@gmail.com>
2024-12-23 19:51:51 +0000

    main

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,25 @@
 console.log("prafful");
 
+//Stack(Primitive) , heap(Non-primitive) 
+
+let myYoutubename = "hteshchoudharydotcom"
+
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo = userOne
+usertwo.email = "hitesh"
+
+console.log(userOne.email);
+console.log(usertwo.email);
