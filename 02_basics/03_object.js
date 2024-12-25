eaf27b2475070502cbb20234e1f882c2fae03965
prafful sharma <sharmaprafful646@gmail.com>
2024-12-25 21:20:29 +0000

    main

---
+++ 02_basics/03_object.js
@@ -0,0 +1,15 @@
+//singleton 
+
+//object literals
+const JsUser = {                  //{} is a object
+    name: "Prafful",
+    age: 18,
+    location: "kota",
+    email: "praffulsarma38@gmail.com",
+    isLoggedIn: false,
+    lastLogginDays: ["Monday", "Saturday"]
+} 
+
+console.log(JsUser.email)
+console.log(JsUser.email)
+
