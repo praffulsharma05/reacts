bfe50ee039491d82876f35bf063e0a6a95dc54de
prafful sharma <sharmaprafful646@gmail.com>
2024-12-25 19:35:07 +0000

    main

--- 02_basics/01_Arrays.js
+++ 02_basics/01_Arrays.js
@@ -491,7 +491,7 @@ the nested objects or arrays (e.g., Array.slice() or spread operator).
 // console.log(original); // [1, 2, { a: 10, b: 20 }]
 // console.log(deepCopy); // [1, 2, { a: 100, b: 20 }]
 
-const myArr = new Array(0,1,2,3,4)
+const myArr = new Array(0,1,2,3,4,5)
 // console.log(myArr[1]);
 // myArr.push(6)
 // myArr.push(7)
@@ -501,12 +501,20 @@ const myArr = new Array(0,1,2,3,4)
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-const newArr = myArr.join()
+// const newArr = myArr.join()
 
-console.log(myArr);
-console.log(newArr);
-console.log(typeof newArr);
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
 
 
+//slice & splice
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3)//not include te range of 3 a/c to this 
 
- 
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)//include the range of 3 a/c to this
+console.log("C", myArr);
+console.log(myn2);
