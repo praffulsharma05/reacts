ab2410942614c4abc2a0c0d9e0e6f6a892f66ad0
prafful sharma <sharmaprafful646@gmail.com>
2024-12-26 17:22:59 +0000

    main

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,8 +1,13 @@
 //singleton 
 
 //object literals
+const mySym = Symbol("key1")
+
+
 const JsUser = {                  //{} is a object
     name: "Prafful",
+    "full name": "prafful sharma",
+    [mySym] : "myKey1",        //[] using this as a sysntax
     age: 18,
     location: "kota",
     email: "praffulsarma38@gmail.com",
@@ -10,6 +15,25 @@ const JsUser = {                  //{} is a object
     lastLogginDays: ["Monday", "Saturday"]
 } 
 
-console.log(JsUser.email)
-console.log(JsUser.email)
+// console.log(JsUser.email)
+// console.log(JsUser["email"])      
+// console.log(JsUser["full name"])
+
+
+// console.log(JsUser[mySym])
+
+
+JsUser.email = "prsfful@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "prafful323@gmail.com"
+// console.log(JsUser);
+JsUser.greeting = function(){
+    console.log("Hello js user"); 
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello js user, ${this.name}`); 
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo( ));
 
