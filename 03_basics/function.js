6ca1aa15d2e94e14a410ff479ec42883970a30d7
prafful sharma <sharmaprafful646@gmail.com>
2024-12-27 16:40:55 +0000

    main

---
+++ 03_basics/function.js
@@ -0,0 +1,21 @@
+//code of multiple line that is packed by package 
+function sayMyName() {
+    
+console.log("h");
+console.log("i");
+console.log("f");
+console.log("f");
+console.log("u");
+console.log("l");
+    
+}
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+const result = addTwoNumbers(3,5)
+
+
+
