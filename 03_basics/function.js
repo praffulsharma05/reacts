dfd0eb8acd782844094da9d559ffc208e97853ee
prafful sharma <sharmaprafful646@gmail.com>
2024-12-28 21:06:42 +0000

    main

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -11,11 +11,28 @@ console.log("l");
 }
 // sayMyName()
 
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
 function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
+    //    let result = number1 + number2
+    //    console.log("prafful");
+
+    //    return result
+
+    return number1 + number2
+       
 }
 
 const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);
 
-
-
+function loginUserMessage(username){
+    if(!username){                                     //
+        console.log("please enter the username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("prafful"))
+console.log(loginUserMessage()); 
