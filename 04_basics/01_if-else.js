162770ca610deaa373a063137ddcfd9a74c3c027
prafful sharma <sharmaprafful646@gmail.com>
2025-01-01 22:27:18 +0000

    main

---
+++ 04_basics/01_if-else.js
@@ -0,0 +1,60 @@
+//if (keyword  hai)
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if(temperature<50){
+//     console.log("less then 50");
+    
+// }else{
+//     console.log("temperature is greater than 50");
+    
+// }
+// console.log("executed");
+
+
+
+//>,<,<=, !=(not equal), ==, ===, 
+// const temperature = 41
+
+// if(temperature === 40){
+//     console.log("valid");
+    
+// }else{
+//     console.log("invalid");
+    
+// }
+// console.log("executed");
+
+
+// const score = 200
+
+// if(score > 100){
+//    let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+//     if(balance<500) {
+//         console.log("test");
+//     }else if(balance <750){
+//         console.log("less than 900");
+//     }else if(balance<900){
+//         console.log("less then 900");
+//     }else{
+//         console.log("less then 1200");
+//     }
+    
+
+// const isUserloggedIn =true 
+// const debitCard = true 
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+// if(isUserloggedIn && debitCard && 3==3){
+//     console.log("Allow to courses");
+// }
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log("user in google");
+// }
