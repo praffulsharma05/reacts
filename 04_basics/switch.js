208938ae3c9519b45ba0f6af851dddd2202ca1ae
prafful sharma <sharmaprafful646@gmail.com>
2025-01-01 22:39:19 +0000

    main

---
+++ 04_basics/switch.js
@@ -0,0 +1,55 @@
+// switch (key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+// const month = 6
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("jane");
+//         break;
+//     case 7:
+//         console.log("july");
+//         break;
+//     case 8:
+//         console.log("augest");
+//         break;
+//     case 9:
+//         console.log("sept");
+//         break;
+//     case 10:
+//         console.log("oct");
+//         break;
+//     case 11:
+//         console.log("nov");
+//         break;
+//     case 12:
+//         console.log("Dec");
+//         break;
+
+//     default:
+//         break;
+// }
+
