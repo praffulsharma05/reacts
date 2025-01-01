ebdf4cc7d8c5af2c2e541949ed850947e9b34952
prafful sharma <sharmaprafful646@gmail.com>
2025-01-01 23:14:37 +0000

    main

---
+++ 04_basics/truthy.js
@@ -0,0 +1,39 @@
+ const userEmail = "hp3@gmail.com"
+
+if (userEmail) {
+    console.log("got user email");
+} else{
+    console.log("Don't have user email");    
+}
+
+//falsy value
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value 
+//"0", 'false', " ",  
+// function(){} empty function bhi truthy value hai
+//[] array bhi truthy value, {} object bhi truthy value hai
+ 
+// if(userEmail.length === 0){
+//     console.log("array is empty");
+    
+// }
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+/*
+false ==0:true
+false =='':true
+0 =='':true
+*/
+
+//Nullish  Coalescing Operator
+
+let val1;
+val1 = 5?? 10
+
+val1 = null ?? 10 ?? 15
+  
+console.log(val1);
